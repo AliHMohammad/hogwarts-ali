205706f6bf7c6a0a7ec6d33b8749f3ab434ebc05
AliHMohammad <alihmohammad@hotmail.com>
2024-02-07 18:44:14 +0100

    part1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import edu.hogwarts.Hogwarts;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Part1");
         Hogwarts hogwarts = new Hogwarts();
         hogwarts.initialize();
     }
